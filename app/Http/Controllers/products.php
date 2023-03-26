756d992b8c89a1ef581cf81f38efbb5d2c13fd31
JavedYcet <70424511+ChJavedIqbal@users.noreply.github.com>
2023-03-26 18:14:28 +0530

    Add files via upload

---
+++ app/Http/Controllers/products.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class products extends Controller
+{
+    //
+    function productlist(){
+        return "product listing";
+    }
+    function addProduct(){
+        return "product added";
+
+    }
+    function updateProduct(){
+        return "product updated";
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\products;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::controller(products::class)->group(function (){
+    Route::get('list','productlist');
+    Route::post('add','addProduct');
+    Route::put('update','updateProduct');
+});
+
+
+
